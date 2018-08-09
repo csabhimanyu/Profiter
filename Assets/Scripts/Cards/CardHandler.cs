28eea1418623892e5f49dfc575e1746a31ccfc4e
pavan_PWL <pavan@plasticwatelabs.com>
2018-08-09 14:43:47 +0530

    Card creation and deletion. Updated new map. Git ignore added.

---
+++ Assets/Scripts/Cards/CardHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardHandler : MonoBehaviour
+{
+    public int cardIndex;
+    public string state;
+    public string item;
+    public int cip;
+    public int qty;
+    public int pricepertonn;
+
+    public Text stateText;
+    public Text itemText;
+    public Text cipText;
+    public Text quantityText;
+    public Text pricepertonneText;
+
+    public void UpdateCardDisplay()
+    {
+        stateText.text = state;
+        itemText.text = item;
+        cipText.text = cip.ToString();
+        quantityText.text = qty.ToString();
+        pricepertonneText.text = pricepertonn.ToString();
+    }
+    public void DestroyCard()
+    {
+        Destroy(this.gameObject);
+    }
+}
+//public int cardIndex;
+//public CardItem cardItem;// Rice=1, Wheat=2, Gram=3,
+//public CreateCard createCard;
+//public int goodsQty; //In Tonnes for M type cards is production & C-type cards its Consumption / Requirement
+//public bool cardValidity; //True or False
+//public int timeInSeconds; //valid for time in seconds
+//public CardCategory cardCategory;//M:MarketPlace, C-City
+//public States state;
+//public int originIndex;
+//public int pricePerTonne; //The goods price per tonne
+//public bool cardCreated;
+//public GameObject cardGameObj;
--- Assets/Scripts/Cards/CardManagement.cs
+++ Assets/Scripts/Cards/CardManagement.cs
@@ -4,47 +4,41 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 
+public enum CardItem { Rice, Wheat, Gram };
+public enum CardCategory { MarketPlace, City };
+public enum States { Bengaluru, Bhopal, Chennai, Delhi, Guwahati, Hyderabad, Jaipur, Kochi, Kolkata, Mumbai, Patna, SriNagar };
+public enum CreateCard { Create, Update, Delete };
+
+[System.Serializable]
+public class ProfiterCardMgmt
+{
+    //Data Definition
+    public int cardIndex;
+    public CardItem cardItem;// Rice=1, Wheat=2, Gram=3,
+    public CreateCard createCard;
+    public int goodsQty; //In Tonnes for M type cards is production & C-type cards its Consumption / Requirement
+    public bool cardValidity; //True or False
+    public int timeInSeconds; //valid for time in seconds
+    public CardCategory cardCategory;//M:MarketPlace, C-City
+    public States state;//Bengaluru, Bhopal, Chennai, Delhi, Guwahati, Hyderabad, Jaipur, Kochi, Kolkata, Mumbai, Patna, SriNagar 
+    public int cip;
+    public int originIndex;
+    public int pricePerTonne; //The goods price per tonne
+    public bool cardCreated;
+    public GameObject cardGameObj;
+}
+
+
 public class CardManagement : MonoBehaviour
 {
-
-    
-    
     public int noOfCards;
-    public class ProfiterCardMgmt
-    {
-        //Data Definition
-        public int cardIndex { get; set; }
-        public int cardItem { get; set; } // Rice=1, Wheat=2, Gram=3,
-        public int goodsQty { get; set; } //In Tonnes for M type cards is production & C-type cards its Consumption / Requirement
-        public bool cardValidity { get; set; } //True or False
-        public int timeInSeconds { get; set; } //valid for time in seconds
-        public char cardCategory { get; set; }//M:MarketPlace, C-City
-        public int originIndex { get; set; }
-        public int pricePerTonne { get; set; } //The goods price per tonne
-        public bool cardCreated { get; set; }
-        public GameObject cardGameObj { get; set; }
-
-        //Default constructor
-        public ProfiterCardMgmt() { }
-
-        public ProfiterCardMgmt(int cardIndex, int cardItem, int goodsQty, bool cardValidity, int timeInSeconds, int originIndex, char cardCategory, int pricePerTonne, GameObject cardGameObj, bool cardCreated = false)
-        {
-            this.cardIndex = cardIndex;
-            this.cardItem = cardItem;
-            this.goodsQty = goodsQty;
-            this.cardValidity = cardValidity;
-            this.timeInSeconds = timeInSeconds;
-            this.originIndex = originIndex;
-            this.cardCategory = cardCategory;
-            this.pricePerTonne = pricePerTonne;
-            this.cardCreated = cardCreated;
-            this.cardGameObj = cardGameObj;
-        }
-    }
-
     //List object which will hold the currently available cards and will be used for all the processing
     public List<ProfiterCardMgmt> updateCardDataList = new List<ProfiterCardMgmt>();
+    public GameObject cardPrefab;
+    public GameObject cardHolder;
+    public int currentActiveIndex;
 
+    public int tempindx;
     /// <summary>
     /// The function will be used to process the card from the backend. This process the card for
     /// 1. createcard
@@ -53,53 +47,49 @@ public class CardManagement : MonoBehaviour
     /// 4. default case has been added to communicate back to the backend to inform the error in the client
     /// </summary>
     /// <param name="jsonObjFromBackend"></param>
-    public void UpdateCard(JObject jsonObjFromBackend)
+    public void UpdateCard()
     {
-        GameObject cardGameObj = new GameObject();
-        switch (jsonObjFromBackend["createcard"].ToString())
+        currentActiveIndex = 0;
+        foreach (var item in updateCardDataList)
         {
-            case "createcard":
-                updateCardDataList.Add(new
-                    ProfiterCardMgmt(
-                        Int32.Parse(jsonObjFromBackend["cardIndex"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["cardItem"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["goodsQty"].ToString()),
-                        bool.Parse(jsonObjFromBackend["cardValidity"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["timeInSeconds"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["originIndex"].ToString()),
-                        Char.Parse(jsonObjFromBackend["cardCategory"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["pricePerTonne"].ToString()),
-                        cardGameObj
-                    ));
-                break;
+            switch (item.createCard)
+            {
+                case CreateCard.Create:
+                    var cardObj = Instantiate(cardPrefab, cardHolder.transform);
+                    cardObj.GetComponent<CardHandler>().state = ((States)item.state).ToString();
+                    cardObj.GetComponent<CardHandler>().cardIndex = item.cardIndex;
+                    cardObj.GetComponent<CardHandler>().item = item.cardItem.ToString();
+                    cardObj.GetComponent<CardHandler>().cip = item.cip;
+                    cardObj.GetComponent<CardHandler>().qty = item.goodsQty;
+                    cardObj.GetComponent<CardHandler>().pricepertonn = item.pricePerTonne;
+                    item.cardCreated = true;
+                    cardObj.SetActive(false);
+                    item.cardGameObj = cardObj;
+                    cardObj.GetComponent<CardHandler>().UpdateCardDisplay();
+                    
+                    break;
 
-            case "updatecard":
-                if (updateCardDataList.RemoveAll(a => a.cardIndex == Int32.Parse(jsonObjFromBackend["cardIndex"].ToString())) == 1)
-                {
-                    updateCardDataList.Add(new
-                        ProfiterCardMgmt(
-                        Int32.Parse(jsonObjFromBackend["cardIndex"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["cardItem"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["goodsQty"].ToString()),
-                        bool.Parse(jsonObjFromBackend["cardValidity"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["timeInSeconds"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["originIndex"].ToString()),
-                        Char.Parse(jsonObjFromBackend["cardCategory"].ToString()),
-                        Int32.Parse(jsonObjFromBackend["pricePerTonne"].ToString()),
-                        cardGameObj
-                    ));
-                }
-                break;
+                case CreateCard.Update:
+                    //itrate throung list sent and update card;
+                    item.cardGameObj.GetComponent<CardHandler>().state = ((States)item.state).ToString();
+                    item.cardGameObj.GetComponent<CardHandler>().item = ((CardItem)item.cardItem).ToString();
+                    item.cardGameObj.GetComponent<CardHandler>().cip = item.cip;
+                    item.cardGameObj.GetComponent<CardHandler>().qty = item.goodsQty;
+                    item.cardGameObj.GetComponent<CardHandler>().pricepertonn = item.pricePerTonne;
+                    item.cardGameObj.GetComponent<CardHandler>().UpdateCardDisplay();
+                    break;
 
-            case "deletecard":
-                //This should destroy the game object as well. Should be tested.
-                DeleteCard(Int32.Parse(jsonObjFromBackend["cardIndex"].ToString()));
-                break;
-            default:
-                Debug.Log("No Matching event found");
-                //Backend communication should be sent for debug purpose and finding any errors in the client should be handled at a later stage
-                break;
+                case CreateCard.Delete:
+                    //This should destroy the game object as well. Should be tested.
+                    DeleteCard(item.cardIndex);
+                    break;
+                default:
+                    Debug.Log("No Matching event found");
+                    //Backend communication should be sent for debug purpose and finding any errors in the client should be handled at a later stage
+                    break;
+            }
         }
+        DisplayUpdate(1);
     }
 
     /// <summary>
@@ -108,45 +98,90 @@ public class CardManagement : MonoBehaviour
     /// </summary>
     /// <param name="cardIndex"></param>
     /// <returns></returns>
-    public bool DeleteCard(int cardIndex)
+    public void DeleteCard(int cardIndex)
     {
-        try
-        {
-            if (updateCardDataList.RemoveAll(a => a.cardIndex == cardIndex) == 1)
-            {
-                return true;
-            }
-        }
-        catch (Exception)
-        {
-            //Comminicate with backend on failure to delete the card to be added
-            throw;
-        }
+        updateCardDataList[cardIndex].cardGameObj.GetComponent<CardHandler>().DestroyCard();
+        updateCardDataList.RemoveAt(cardIndex);
+              DisplayUpdate(0);
         
-        return false;
     }
 
-    private void CardPostion() {
-    
+
+    private void CardPostion()
+    {
+
     }
-
-	// Use this for initialization
-	private void Start () {
-            
-            
-            // not required???
-
-        }
-	private void Update()
-	{
+   
+    // Use this for initialization
+    private void Start()
+    {
+        currentActiveIndex = 0;
+        // not required???
+        UpdateCard();
+    }
+    private void Update()
+    {
 
         // scan the carMgmt List  for which gameobject is not created 
         // Create card type prefab game objects  
         // call the CardPosition() function to identiy the position for this card
         // destroy any old cards that need to deleted 
         //
-        
-	
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DeleteCard(tempindx);
+        }
 
-	}
+
+    }
+
+    public void DisplayUpdate(int dispValue)
+    {
+        int count = 0;
+        if (dispValue == 1)
+        {
+            for (int i = 0; i < updateCardDataList.Count; i++)
+            {
+                updateCardDataList[i].cardGameObj.gameObject.SetActive(false);
+                if (i == currentActiveIndex && count < 4)
+                {
+                    updateCardDataList[i].cardGameObj.gameObject.SetActive(true);
+                    count++;
+                    currentActiveIndex++;
+                    if(currentActiveIndex >= updateCardDataList.Count)
+                    {
+                        currentActiveIndex = 0;
+                    }
+                }
+
+            }
+        }
+        else
+        {
+            for (int i = (updateCardDataList.Count-1); i >=0; i--)
+            {
+                updateCardDataList[i].cardGameObj.gameObject.SetActive(false);
+                if (i == currentActiveIndex && count < 4)
+                {
+                    updateCardDataList[i].cardGameObj.gameObject.SetActive(true);
+                    count++;
+                    currentActiveIndex--;
+                    if (currentActiveIndex < 0)
+                    {
+                        currentActiveIndex = (updateCardDataList.Count-1);
+                    }
+                    
+                }
+
+            }
+        }
+    }
+    public void Next()
+    {
+       DisplayUpdate(1);
+    }
+    public void Prev()
+    {
+       DisplayUpdate(0);
+    }
 }
